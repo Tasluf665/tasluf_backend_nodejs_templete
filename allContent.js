ba001989afbcd58a700f444e4d4ae4a1dddc02bf
Tasluf665 <tasluf15-12089@diu.edu.bd>
2024-10-11 23:51:33 +0200

    Complete version 1

---
+++ allContent.js
@@ -0,0 +1,108 @@
+const asyncContent = `module.exports = function (handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    } catch (ex) {
+      next(ex);
+    }
+  };
+};
+`
+
+const errorContent = `module.exports = function (err, req, res, next) {
+  res.status(500).send("Something failed");
+};
+`
+
+const gitignoreContent = `# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+.vercel
+`
+
+const envContent = `PORT=
+BASE_URL=
+
+MONGODB_URL=
+`
+
+const indexContent = `const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require('express');
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const error = require("./middleware/error");
+
+//Write the routes
+// const auth = require("./routes/auth");
+
+const app = express();
+app.set("view engine", "ejs");
+
+if (!process.env.MONGODB_URL) {
+    console.error("FATAL ERROR: MONGODB_URL is not define");
+    process.exit(1);
+}
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected with mongodb"))
+    .catch((err) => console.log("Could not connect to mongodb", err));
+
+//Write all the origins for corsOptions    
+// const origins = [
+//     "http://localhost:3000",
+// ];
+
+const corsOptions = {
+    origin: origins,
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+//Write the routes
+app.use("/api/auth", auth);
+
+app.use(error);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log('Listening on port ', port, "..."));
+
+`
+
+module.exports = {
+    asyncContent,
+    errorContent,
+    gitignoreContent,
+    envContent,
+    indexContent
+};
---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+const projectRoot = path.resolve(__dirname);
+const allContent = require('./allContent');
+
+const createFiles = (folderPath, fileName, content) => {
+    const dir = path.join(projectRoot, folderPath);
+    const filePath = path.join(dir, fileName);
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    fs.writeFileSync(filePath, content);
+}
+
+const createFolder = (folderPath) => {
+    const dir = path.join(projectRoot, folderPath);
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+}
+
+const installDependencies = () => {
+    console.log('Installing dependencies...');
+
+    // Run the npm install command using child_process
+    exec('npm install express cors dotenv ejs joi joi-objectid lodash mongoose', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error during npm install: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log('Dependencies installed successfully!');
+    });
+}
+
+
+
+const main = () => {
+    createFiles("middleware", "async.js", allContent.asyncContent)
+    createFiles("middleware", "error.js", allContent.errorContent)
+    createFolder("models")
+    createFolder("routes")
+    createFolder("utils")
+    createFolder("views")
+    createFiles("", ".env", allContent.envContent)
+    createFiles("", ".gitignore", allContent.gitignoreContent)
+    createFiles("", "app.js", allContent.indexContent)
+
+    installDependencies()
+};
+
+main();
